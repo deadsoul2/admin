06eed7ac4c926ab677ec1be3bb8abb147bbf8637
deadsoul2 <45014056+deadsoul2@users.noreply.github.com>
2018-11-14 15:27:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '-'
-status = idle
+const status=idle
 
 client.on('ready', function() {
     console.log(`i am ready ${client.user.username}`);
